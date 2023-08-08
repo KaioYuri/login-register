371cc00123029032bdcb503520ac6645ea8d62bf
KaioYuri <kaioyuri96@gmail.com>
2023-08-08 12:53:47 -0300

    Adição de registro e service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private builder: FormBuilder,private toastr:ToastrService,
+    private service:AuthService,private router:Router) {
+
+  }
+
+  registerform=this.builder.group({
+    id:this.builder.control('',Validators.compose([Validators.required,Validators.minLength(5)])),
+    name:this.builder.control('',Validators.required),
+    password:this.builder.control('',Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])),
+    email:this.builder.control('',Validators.compose([Validators.required, Validators.email])),
+    gender:this.builder.control('male'),
+    role:this.builder.control(''),
+    insactive:this.builder.control(false)
+  });
+
+  proceedregisteration(){
+    if(this.registerform.valid){
+      this.service.Proceedregister(this.registerform.value).subscribe(res => {
+        this.toastr.success('Por favor, contate o admin para permitir acesso','Registrado com sucesso')
+        this.router.navigate(['login']);
+      });
+    } else {
+      this.toastr.warning('Por favor, entre com dados validos');
+    }
+  }
 }
