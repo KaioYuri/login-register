53c1ca83fc0e64590678653059044826ce0e7f95
KaioYuri <kaioyuri96@gmail.com>
2023-08-09 12:49:43 -0300

    Adição do login reativo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private builder: FormBuilder,private toastr:ToastrService,
+    private service:AuthService,private router:Router) {
 
+  }
+
+  userdata:any;
+
+  loginform=this.builder.group({
+    username:this.builder.control('',Validators.required),
+    password:this.builder.control('',Validators.required)
+  })
+
+  proceedlogin(){
+    if(this.loginform.valid){
+    //   this.service.Proceedregister(this.loginform.value).subscribe(res => {
+    //     this.toastr.success('Você entrou!','Logado com sucesso')
+    //     this.router.navigate(['home']);
+    //   });
+    // } else {
+    //   this.toastr.warning('Por favor, entre com dados validos');
+    // }
+    this.service.Getbycode(this.loginform.value.username).subscribe(res => {
+      this.userdata=res;
+      console.log(this.userdata);
+      if(this.userdata.password===this.loginform.value.password){
+        if(this.userdata.isactive){
+          sessionStorage.setItem('username', this.userdata.id);
+          sessionStorage.setItem('userrole', this.userdata.role);
+          this.router.navigate(['']);
+        }else{
+          this.toastr.error('Por favor, fale com o admin', ' Usuário está ativo');
+        }
+      }else{
+        this.toastr.error('Credenciais inválidas');
+      }
+    })
+    }
+  }
 }
