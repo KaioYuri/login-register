e23d39a98dbd1dfb4aff36202ab86852e0d8e9c6
KaioYuri <kaioyuri96@gmail.com>
2023-08-15 10:28:26 -0300

    Adição da Autenticação e rotas

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,31 @@
-import { CanActivateFn } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | UrlTree | boolean {
+    if (this.authService.IsloggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,45 +7,45 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private builder: FormBuilder,private toastr:ToastrService,
-    private service:AuthService,private router:Router) {
-
+  constructor(
+    private builder: FormBuilder,
+    private toastr: ToastrService,
+    private service: AuthService,
+    private router: Router
+  ) {
+    sessionStorage.clear();
   }
 
-  userdata:any;
+  userdata: any;
 
-  loginform=this.builder.group({
-    username:this.builder.control('',Validators.required),
-    password:this.builder.control('',Validators.required)
-  })
+  loginform = this.builder.group({
+    username: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.required),
+  });
 
-  proceedlogin(){
-    if(this.loginform.valid){
-    //   this.service.Proceedregister(this.loginform.value).subscribe(res => {
-    //     this.toastr.success('Você entrou!','Logado com sucesso')
-    //     this.router.navigate(['home']);
-    //   });
-    // } else {
-    //   this.toastr.warning('Por favor, entre com dados validos');
-    // }
-    this.service.Getbycode(this.loginform.value.username).subscribe(res => {
-      this.userdata=res;
-      console.log(this.userdata);
-      if(this.userdata.password===this.loginform.value.password){
-        if(this.userdata.isactive){
-          sessionStorage.setItem('username', this.userdata.id);
-          sessionStorage.setItem('userrole', this.userdata.role);
-          this.router.navigate(['']);
-        }else{
-          this.toastr.error('Por favor, fale com o admin', ' Usuário está ativo');
+  proceedlogin() {
+    if (this.loginform.valid) {
+      this.service.Getbycode(this.loginform.value.username).subscribe((res) => {
+        this.userdata = res;
+        console.log(this.userdata);
+        if (this.userdata.password === this.loginform.value.password) {
+          if (this.userdata.isactive) {
+            sessionStorage.setItem('username', this.userdata.id);
+            sessionStorage.setItem('userrole', this.userdata.role);
+            this.router.navigate(['']);
+          } else {
+            this.toastr.error(
+              'Por favor, fale com o admin',
+              ' Usuário está ativo'
+            );
+          }
+        } else {
+          this.toastr.error('Credenciais inválidas');
         }
-      }else{
-        this.toastr.error('Credenciais inválidas');
-      }
-    })
+      });
     }
   }
 }
