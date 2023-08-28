fbb55a6e0049fde32f19ef17c664a9945c9003f1
KaioYuri <kaioyuri96@gmail.com>
2023-08-28 09:13:26 -0300

    feat: navbar only for users

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -8,15 +8,19 @@ import {
 import { AuthService } from '../service/auth.service';
 import { Observable } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthGuard {
+  
   constructor(
     private authService: AuthService,
     private router: Router,
     private toastr: ToastrService
   ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
